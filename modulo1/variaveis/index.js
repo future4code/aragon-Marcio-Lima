16107ebc09ca0bba480e8d5522c9d311b3b7bfc1
Marcio Lima <mvl-2007@hotmail.com>
2022-03-21 20:47:13 -0300

    incluindo-exercícios-sobre-variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,70 @@
+/* Exercícios de interpretação de código:
+Exercício 1:
+
+O primeiro console vai imprimir: 10
+
+O segundo console vai imprimir: 10 5
+
+Exercício 2:
+
+let a = 10
+let b = 20
+let c = a // 10
+b = c // 10
+a = b // 10
+
+console.log(a, b, c) //Vai imprimir: 10 10 10
+
+Exercício 3:
+
+let horasTrabalhadasPorDia = prompt("Quantas horas você trabalha por dia?")
+let valorRecebidoPorDia = prompt("Quanto você recebe por dia?")
+console.log("Voce trabalha diariamente", horasTrabalhadasPorDia, "horas e recebe", valorRecebidoPorDia, "reais por dia")
+
+*/
+
+//Exercício escrita de código
+//Exerício 1:
+
+let nome
+let idade
+console.log (typeof nome)
+console.log (typeof idade)
+
+//Como não havia valor atribuído ele retornou: undefined
+
+nome = prompt ("Qual o seu nome?")
+idade = prompt ("Quantos anos você tem?")
+
+console.log (typeof nome)
+console.log (typeof idade)
+
+//O tipo das variáveis nome e idade retornou: string
+
+console.log ("Olá", nome, "você tem", idade, "anos")
+
+//Exercício 2:
+
+let pergunta1 = "Você estuda programação?"
+let pergunta2 = "Você tem filhos?"
+let pergunta3 = "Você já viajou para Europa?"
+
+let resposta1 = prompt (pergunta1)
+let resposta2 = prompt (pergunta2)
+let resposta3 = prompt (pergunta3)
+
+console.log (pergunta1, " - ", resposta1)
+console.log (pergunta2, " - ", resposta2)
+console.log (pergunta3, " - ", resposta3)
+
+//Exercício 3:
+
+
+
+
+
+
+
+
+
+
