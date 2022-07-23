40d2ec49eb93bb92eaa2ea1a3ff1e463fc2d0c19
marcio <mvl-2007@hotmail.com>
2022-07-22 21:28:11 -0300

    Feat: Alterando algumas partes do endpoint 6

--- modulo4/labecommerce-backend/src/endpoints/getPurchaseByUserId.ts
+++ modulo4/labecommerce-backend/src/endpoints/getPurchaseByUserId.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express"
 import connection from "../database/connection"
-import { TABLE_PURCHASES, TABLE_USERS } from "../database/tableNames"
+import { TABLE_USERS } from "../database/tableNames"
 
 export const getUserPurchases = async (req: Request, res: Response) => {
     let errorCode = 400
     try {
-        const id = req.params.id
+        const user_id = req.params.user_id
 
         const idChecker = await connection(TABLE_USERS)
             .select()
-            .where("id", "=", `${id}`)
+            .where("id", "=", `${user_id}`)
 
         if (idChecker.length === 0) {
             errorCode = 404
@@ -17,16 +17,20 @@ export const getUserPurchases = async (req: Request, res: Response) => {
         }
 
         const result = await connection.raw(`
-        SELETC
-        name, price
-        FROM Labe_Products
-        JOIN Labe_Purchases
+        SELECT
+        Labe_Users.email,
+        Labe_Products.name,
+        Labe_Products.price,
+        Labe_Purchases.quantity,
+        Labe_Purchases.total_price
+        FROM Labe_Purchases
+        JOIN Labe_users
+        ON Labe_Purchases.user_id = Labe_users.id
+        JOIN Labe_Products
         ON Labe_Purchases.product_id = Labe_Products.id
-        WHERE Labe_purchases.user_id = 
+        WHERE Labe_Purchases.user_id = ${user_id};
         `)
-
         res.status(200).send({
-            message: "",
             result: result,
         })
     } catch (error) {
