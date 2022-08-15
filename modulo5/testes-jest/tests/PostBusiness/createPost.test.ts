d2351e73768407a82f4fe866847654c1d8769170
marcio <mvl-2007@hotmail.com>
2022-08-15 17:13:40 -0300

    feat: Adicionando exercícios de testes

---
+++ modulo5/testes-jest/tests/PostBusiness/createPost.test.ts
@@ -0,0 +1,28 @@
+import { PostBusiness } from "../../src/business/PostBusiness"
+import { PostDatabase } from "../../src/database/PostDatabase"
+import { ICreatePostInputDTO } from "../../src/models/Post"
+import { PostDatabaseMock } from "../mocks/PostDatabaseMock"
+import { AuthenticatorMock } from "../mocks/services/AuthenticatorMock"
+import { HashManagerMock } from "../mocks/services/HashManagerMock"
+import { IdGeneratorMock } from "../mocks/services/IdGeneratorMock"
+
+describe("Testando PostBusiness", () => {
+    const postBusiness = new PostBusiness(
+        new PostDatabaseMock() as unknown as PostDatabase,
+        new IdGeneratorMock(),
+        new HashManagerMock(),
+        new AuthenticatorMock()
+    )
+
+    test("criação de post bem sucedida", async () => {
+        const input: ICreatePostInputDTO = {
+            token: "token-mock",
+            content: "create-test",
+        }
+
+        const response = await postBusiness.createPost(input)
+
+        expect(response.message).toEqual("Post criado com sucesso")
+        expect(response.post.getContent()).toEqual("create-test")
+    })
+})
